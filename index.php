bde142344c825362ad733de0d4258b093c0aef79
前林俊希 <toshiki@maehayashishunnozominoMacBook-Pro.local>
2019-11-20 19:55:18 +0900

    最初のpush

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Zipangu Cannabis </title>
+  <link rel="stylesheet" href="style/css/common.css?<?php echo date('Ymd-Hi'); ?>" type="text/css">
+  <link rel="stylesheet" href="style/css/style.css?<?php echo date('Ymd-Hi'); ?>" type="text/css">
+  <link rel="stylesheet" href="style/css/style_sp.css?<?php echo date('Ymd-Hi'); ?>" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+  <div id="wraper">
+    <header>
+      <div class="Header"></div>
+    </header>
+    <div class="MainContainer-mv">
+      <div class="MainContainer-mv-title">
+        <h2 class="MainContainer-mv-title__content pc">THE BEST CHILLING FOR YOU</h2>
+        <h2 class="MainContainer-mv-title__content sp"><img class="eyecatch" src="images/FV_catch.png"></h2>
+      </div>
+      <div class="MainContainer-mv-btn">
+        <a class="MainContainer-mv-btn__cta" href="commingsoon.html">For PROFESSIONAL ></a>
+        <a class="MainContainer-mv-btn__cta bg-white" href="commingsoon.html">For RELAX ></a>
+      </div>
+    </div>
+    <div class="MainContainer-what">
+      <div class="MainContainer-what-img"><img src="images/what_image.jpg"></div>
+      <div class="MainContainer-what-details">
+        <p class="MainContainer-what-details__title"><span>充実した人生と中途半端な人生。<br></span><span>その差は一体なんだ。</span><br><span>最高の「集中」と「休息」をあなたに。</span></p>
+        <p class="MainContainer-what-details__text">一人ひとりで異なる声やニーズを汲み取り、あなたに理想の製品を提案します。<br>原産地や生産元とコミュニケーションを綿密に取り、私たちが実際に体験しています。<br>その際、妥協を許さず、徹底的に品質にこだわり、厳選した物のみを取り揃えています。洗練された製品を、あなたにお届けします。あなたの用途に合った製品でより良いライフスタイルをお送りください。</p>
+        <p class="MainContainer-what-details__message">The best chilling for you.</p>
+        <p class="more"><a href="commingsoon.html"><span>VIEW MORE</span></a></p>
+      </div>
+    </div>
+    <div class="MainContainer-product">
+      <div class="MainContainer-title">
+        <h2>PRODUCT</h2>
+      </div>
+      <div class="works-list horizontal_scroll_wrap">
+        <ul class="scroll_lst">
+          <li class="scroll_item">
+            <a href="" class="scroll_item_inr">
+              <div class="scroll_item_thum">
+              </div>
+            </a>
+          </li>
+          <li class="scroll_item">
+            <a href="" class="scroll_item_inr">
+              <div class="scroll_item_thum">
+              </div>
+            </a>
+          </li>
+          <li class="scroll_item">
+            <a href="" class="scroll_item_inr">
+              <div class="scroll_item_thum">
+              </div>
+            </a>
+          </li>
+          <li class="scroll_item">
+            <a href="" class="scroll_item_inr">
+              <div class="scroll_item_thum">
+              </div>
+            </a>
+          </li>
+          <li class="scroll_item">
+            <a href="" class="scroll_item_inr">
+              <div class="scroll_item_thum">
+              </div>
+            </a>
+          </li>
+          <li class="scroll_item">
+            <a href="" class="scroll_item_inr">
+              <div class="scroll_item_thum">
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="MainContainer-product-for">
+        <div class="for__people for__people-professional">
+          <a href="commingsoon.html">
+            <div class="for__people-text">For PROFESSIONAL</div>
+          </a>
+        </div>
+        <div class="for__people for__people-chill">
+          <a href="commingsoon.html">
+            <div class="for__people-text">For RELAX</div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="MainContainer-aboutus">
+      <div class="about-company__details__inner">
+        <div class="MainContainer-title">
+          <h2>ABOUT US</h2>
+        </div>
+        <dl>
+          <dt>事業体</dt>
+          <dd>Zipangu Cannabis</dd>
+          <dt>設立年</dt>
+          <dd>2019年</dd>
+          <dt>代表</dt>
+          <dd>井上 悠</dd>
+          <dt>共同代表</dt>
+          <dd>中島 惇生</dd>
+          <dt>資本金</dt>
+          <dd>100万円</dd>
+          <dt>事業内容</dt>
+          <dd>CBD製品の輸入/製造販売およびグローバルでのカナビス関連事業</dd>
+        </dl>
+      </div>
+    </div>
+    <div class="MainContainer-contact">
+      <a href="commingsoon.html">
+        <div class="contact-wrapper">
+          <div class="contact-thumb"></div>
+          <div class="contact-text__content">
+            <div class="contact-text__title c-text__bold">Contact Us</div>
+          </div>
+          <span class="p-top__special_arrow"></span>
+        </div>
+      </a>
+    </div>
+  </div>
+</body>
+
+</html>
